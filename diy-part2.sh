06517264c83c952faa8f265692348ad9633c1b4b
lxhao61 <lxhao61@gmail.com>
2023-03-20 13:11:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,7 +29,7 @@ git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/
 #git clone -b packages https://github.com/lxhao61/openwrt-passwall.git package/xiaorouji/packages
 #git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/luci
 #git clone -b luci-reality https://github.com/lxhao61/openwrt-passwall.git package/xiaorouji/luci
-git clone -b luci-dev https://github.com/nftbty/openwrt-passwall.git package/xiaorouji/luci
+git clone -b luci-test https://github.com/lxhao61/openwrt-passwall.git package/xiaorouji/luci
 
 # 拉取ShadowSocksR Plus+源码
 #git clone -b master https://github.com/fw876/helloworld.git package/helloworld
