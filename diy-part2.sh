82f1ec04b7a8a25bbf1e262357880b4c73f1a2f8
lxhao61 <lxhao61@gmail.com>
2023-03-22 19:28:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,9 +27,8 @@ rm -rf package/feeds/packages/xray-core
 # 拉取PassWall源码
 git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/packages
 #git clone -b packages https://github.com/lxhao61/openwrt-passwall.git package/xiaorouji/packages
-#git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/luci
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/luci
 #git clone -b luci-reality https://github.com/lxhao61/openwrt-passwall.git package/xiaorouji/luci
-git clone -b luci-test https://github.com/lxhao61/openwrt-passwall.git package/xiaorouji/luci
 
 # 拉取ShadowSocksR Plus+源码
 #git clone -b master https://github.com/fw876/helloworld.git package/helloworld
