4628c9f1fbb90f6177fc19d371c9decc17298079
lxhao61 <lxhao61@gmail.com>
2023-09-09 04:53:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,6 +17,7 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 
 # 删除lienol大自带xray-core核心
 rm -rf package/feeds/packages/xray-core
+rm -rf feeds/packages/net/xray-core
 
 # 拉取PassWall源码
 git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/xiaorouji/packages
