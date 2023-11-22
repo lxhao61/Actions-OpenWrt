d5f005751face266a355c9e058258cebb096733a
lxhao61 <lxhao61@gmail.com>
2023-11-22 10:44:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,11 +14,17 @@
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
-# 删除自带xray-core源码
+# 删除自带 golang 源码
+rm -rf feeds/packages/lang/golang
+
+# 拉取 golang 源码
+svn co https://github.com/coolsnowwolf/packages/trunk/lang/golang feeds/packages/lang/golang
+
+# 删除自带 xray-core 源码
 rm -rf feeds/packages/net/xray-core
 rm -rf package/feeds/packages/xray-core
 
-# 拉取PassWall源码
+# 拉取 PassWall 源码
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/feeds/packages/passwall
 #git clone -b packages https://github.com/lxhao61/openwrt-passwall.git package/feeds/packages/passwall
 #cd package/feeds/packages/passwall
@@ -30,19 +36,19 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/feeds/luci/l
 #git checkout d1e618220a9a0a4b73d536101f452a2f4cf14861
 #cd -
 
-# 拉取ShadowSocksR Plus+源码
+# 拉取 ShadowSocksR Plus+ 源码
 #git clone -b master https://github.com/fw876/helloworld.git package/feeds/helloworld
 
-# 拉取msd_lite源码
-git clone https://github.com/ximiTech/msd_lite.git package/feeds/packages/msd_lite
-git clone https://github.com/ximiTech/luci-app-msd_lite.git package/feeds/luci/luci-app-msd_lite
+# 拉取 tailscale 源码
+svn co https://github.com/coolsnowwolf/packages/trunk/net/tailscale package/feeds/packages/tailscale
 
-# 拉取官方phtunnel、pgyvpn源码
+# 拉取 phtunnel、pgyvpn 源码
 #git clone https://github.com/OrayOS/OpenOray.git package/feeds/OpenOray
 
-# 拉取lean大phtunnel源码
+# 拉取 phtunnel 源码
 svn co https://github.com/coolsnowwolf/packages/trunk/net/phtunnel package/feeds/packages/phtunnel
 svn co https://github.com/OrayOS/OpenOray/trunk/luci-app-phtunnel package/feeds/luci/luci-app-phtunnel
 
-# 拉取lean大tailscale源码
-#svn co https://github.com/coolsnowwolf/packages/trunk/net/tailscale package/feeds/packages/tailscale
+# 拉取 msd_lite 源码
+git clone https://github.com/ximiTech/msd_lite.git package/feeds/packages/msd_lite
+git clone https://github.com/ximiTech/luci-app-msd_lite.git package/feeds/luci/luci-app-msd_lite
