1b6184849850121a039280bb104b52947f00a080
lxhao61 <lxhao61@gmail.com>
2023-11-27 00:47:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -50,7 +50,6 @@ svn co https://github.com/coolsnowwolf/packages/trunk/net/phtunnel package/feeds
 
 # 提取 luci-app-phtunnel 源码
 svn co https://github.com/OrayOS/OpenOray/trunk/luci-app-phtunnel package/feeds/luci/luci-app-phtunnel
-#svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-phtunnel package/feeds/luci/luci-app-phtunnel
 
 # 拉取 msd_lite 源码
 git clone https://github.com/ximiTech/msd_lite.git package/feeds/packages/msd_lite
