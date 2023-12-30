c1710c61defa8c8e68a19c53c5fbd9ad5321e88e
lxhao61 <lxhao61@gmail.com>
2023-12-30 09:00:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,12 +14,6 @@
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
-# 删除自带 golang 源码
-#rm -rf feeds/packages/lang/golang
-
-# 提取 golang 源码
-#svn co https://github.com/coolsnowwolf/packages/trunk/lang/golang feeds/packages/lang/golang
-
 # 删除自带 xray-core 源码
 rm -rf feeds/packages/net/xray-core
 rm -rf package/feeds/packages/xray-core
@@ -38,9 +32,6 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/feeds/luci/l
 
 # 拉取 ShadowSocksR Plus+ 源码
 #git clone -b master https://github.com/fw876/helloworld.git package/feeds/helloworld
-
-# 提取 tailscale 源码
-#svn co https://github.com/coolsnowwolf/packages/trunk/net/tailscale feeds/packages/net/tailscale
 
 # 拉取 phtunnel、pgyvpn 源码
 #git clone https://github.com/OrayOS/OpenOray.git package/feeds/OpenOray
