edfe489149d641ca1d70589a25645b8912fd7161
lxhao61 <lxhao61@gmail.com>
2024-01-25 08:51:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,6 +10,9 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
+# 回退源码
+#git reset --hard 5242189
+
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
