15377a6165ed725d68c8f2231f0ef962713f31e9
lxhao61 <lxhao61@gmail.com>
2024-01-25 08:54:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,41 +14,40 @@
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
-# 删除自带 golang 源码
+# 删除自带 golang
 rm -rf feeds/packages/lang/golang
 
-# 拉取 golang 源码
+# 拉取 golang
 git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 
-# 删除自带 xray-core 源码
+# 删除自带 xray-core
 rm -rf feeds/packages/net/xray-core
 rm -rf package/feeds/packages/xray-core
 
-# 拉取 passwall-packages 源码
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/feeds/packages/passwall
-#cd package/feeds/packages/passwall
+# 拉取 passwall-packages
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
+#cd package/passwall/packages
 #git checkout c189a68728d6bb65d9fb4b47fdacea3ba970a624
 #cd -
 
-# 拉取 luci-app-passwall 源码
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/feeds/luci/luci-app-passwall
-#cd package/feeds/luci/luci-app-passwall
+# 拉取 luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci-app-passwall
+#cd package/passwall/luci-app-passwall
 #git checkout d1e618220a9a0a4b73d536101f452a2f4cf14861
 #cd -
 
-# 拉取 ShadowSocksR Plus+ 源码
+# 拉取 ShadowSocksR Plus+
 #git clone -b master https://github.com/fw876/helloworld.git package/helloworld
 
-# 拉取 msd_lite 源码
-git clone https://github.com/ximiTech/msd_lite.git package/feeds/packages/msd_lite
-git clone https://github.com/ximiTech/luci-app-msd_lite.git package/feeds/luci/luci-app-msd_lite
+# 拉取 msd_lite
+git clone https://github.com/ximiTech/msd_lite.git package/msd_lite/msd_lite
+git clone https://github.com/ximiTech/luci-app-msd_lite.git package/msd_lite/luci-app-msd_lite
 
-# 拉取 phtunnel、pgyvpn 源码
+# 拉取 phtunnel、pgyvpn
 #git clone https://github.com/OrayOS/OpenOray.git package/OpenOray
 
-# 删除自带 tailscale 源码
+# 删除自带 tailscale
 rm -rf feeds/packages/net/tailscale
-#rm -rf package/feeds/packages/tailscale
 
 # 筛选程序
 function merge_package(){
@@ -70,4 +69,4 @@ function merge_package(){
     cd "$rootdir"
 }
 # 提取 tailscale 源码
-merge_package main https://github.com/kenzok8/small-package feeds/packages/net/tailscale tailscale
+merge_package main https://github.com/kenzok8/small-package feeds/packages/net tailscale
