520882e1f96906749399040879afc73df2fd9b4f
lxhao61 <lxhao61@gmail.com>
2024-01-26 11:28:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,13 +14,13 @@
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
-# 删除自带 golang 源码
+# 删除自带 golang
 #rm -rf feeds/packages/lang/golang
 
-# 拉取 golang 源码
+# 拉取 golang
 #git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
 
-# 删除自带 xray-core 源码
+# 删除自带 xray-core
 rm -rf feeds/packages/net/xray-core
 rm -rf package/feeds/packages/xray-core
 
@@ -36,9 +36,9 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luc
 #git checkout d1e618220a9a0a4b73d536101f452a2f4cf14861
 #cd -
 
-# 拉取ShadowSocksR Plus+源码
+# 拉取 ShadowSocksR Plus+
 #git clone -b master https://github.com/fw876/helloworld.git package/helloworld
 
-# 拉取 msd_lite 源码
+# 拉取 msd_lite
 git clone https://github.com/ximiTech/msd_lite.git package/feeds/packages/msd_lite
 git clone https://github.com/ximiTech/luci-app-msd_lite.git package/feeds/luci/luci-app-msd_lite
