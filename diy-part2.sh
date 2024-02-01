9dcd4f563ddb32e5110924e3f9aadf89212036a8
lxhao61 <lxhao61@gmail.com>
2024-02-02 05:47:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -43,6 +43,9 @@ git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luc
 git clone https://github.com/ximiTech/msd_lite.git package/msd_lite/msd_lite
 git clone https://github.com/ximiTech/luci-app-msd_lite.git package/msd_lite/luci-app-msd_lite
 
+# 拉取 OpenAppFilter、luci-app-oaf
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
 # 拉取 phtunnel、pgyvpn
 #git clone https://github.com/OrayOS/OpenOray.git package/OpenOray
 
