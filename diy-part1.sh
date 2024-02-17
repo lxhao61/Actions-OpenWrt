e92fdf45f580dac3bb88eb390435ebb2ee907847
lxhao61 <lxhao61@gmail.com>
2024-02-17 11:08:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,9 +19,9 @@ git reset --hard 59fd8f0
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # 注释默认 packages
-#sed -i 's/^\(.*packages\)/#&/' feeds.conf.default
+sed -i 's/^\(.*packages\)/#&/' feeds.conf.default
 # 添加回退 packages
-#sed -i '$a src-git packages https://github.com/Lienol/openwrt-packages.git^fb21629' feeds.conf.default
+sed -i '$a src-git packages https://github.com/Lienol/openwrt-packages.git^99c85b7' feeds.conf.default
 
 # 注释默认 luci
 #sed -i 's/^\(.*luci\)/#&/' feeds.conf.default
