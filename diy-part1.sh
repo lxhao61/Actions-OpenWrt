f5aaf9102b2cfce11c9fc8b09d6096f9936d7224
haoran <lxhao61@gmail.com>
2026-09-06 07:08:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,8 +12,8 @@
 
 # 查看所有标签
 #git tag
-# 切换到标签 v24.10.8
-git checkout v24.10.8
+# 切换到标签 v23.05.6
+git checkout v23.05.6
 
 # 全部改为稳定的 github 源
 sed -i 's|https://git.openwrt.org/feed/packages.git|https://github.com/openwrt/packages.git|g' feeds.conf.default
@@ -22,7 +22,7 @@ sed -i 's|https://git.openwrt.org/feed/routing.git|https://github.com/openwrt/ro
 sed -i 's|https://git.openwrt.org/feed/telephony.git|https://github.com/openwrt/telephony.git|g' feeds.conf.default
 
 # 添加 lienol 大的 package
-echo 'src-git lienol https://github.com/Lienol/openwrt-package.git;main' >>feeds.conf.default
+echo 'src-git lienol https://github.com/Lienol/openwrt-package.git;lua' >>feeds.conf.default
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
